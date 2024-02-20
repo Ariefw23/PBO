da7a3ac46dc5f19446432762a7a3561165947920
Arief Wibowo <ariefw026@gmail.com>
2024-02-20 17:27:47 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.sun.source.tree.NewArrayTree;
+
+import java.time.LocalDate;
+import java.time.Period;
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String Nama;
+        String JenisKelamin;
+        String Tanggal;
+        Scanner objinput = new Scanner(System.in);
+
+        System.out.print("Nama : ");
+        Nama = objinput.nextLine();
+        System.out.print("Jenis Kelamin (L/P) : ");
+        JenisKelamin = objinput.nextLine();
+        System.out.print("Tanggal (yyyy/mm/dd) : ");
+        Tanggal = objinput.nextLine();
+
+        LocalDate birthdate = LocalDate.parse(Tanggal);
+
+        LocalDate today = LocalDate.now();
+
+        Period period = Period.between(birthdate, today);
+        int years = period.getYears();
+        int mounth = period.getMonths();
+
+        System.out.println("Nama : " + Nama);
+        System.out.println("Jenis Kelamin : " + JenisKelamin);
+        System.out.println("Umur Anda : " + years + " Tahun" + mounth + " Bulan");
+
+    }
+}
+
+
+
+
